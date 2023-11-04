7d07a21ee2bdd965468b00a720dcbaa458450697
NlSEMONO <fmadarang2542@hotmail.com>
2023-11-04 14:29:52 -0400

    Finshed frontend prototype

--- vid-notes-page/app/components/Navbar.tsx
+++ vid-notes-page/app/components/Navbar.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const navbarSettings = 'h-16 mb-8 bg-main p-4';
+    const headerContainer = 'flex flex-row w-3/12 justify-between m-auto';
+    const textSettings = 'text-white text-lg';
+    return (
+        <div className={`${navbarSettings}`}>
+            <div className={`${headerContainer}`}>
+                <h1 className={`${textSettings}`}> Home </h1>
+                <a href='/#usage'> 
+                    <h1 className={`${textSettings}`}> Usage </h1>
+                </a>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
--- vid-notes-page/app/components/SubmitLink.tsx
+++ vid-notes-page/app/components/SubmitLink.tsx
@@ -1,16 +1,33 @@
 'use client'
 
-import React from 'react'
+import {useState} from 'react'
+
+const HOST = 'http://localhost:8000';
 
 const SubmitLink = () => {
-    const containerSettings = 'mt-10 w-9/12 m-auto flex justify-between';
-    const btnSettings = 'border p-2 px-8';
+    const containerSettings = 'mt-10 w-7/12 m-auto flex justify-between';
+    const fieldSettings = 'w-7/12 h-10 border-2';
+    const btnSettings = 'border p-2 px-8 bg-main h-10 text-white';
+    const [notes, setNotes] = useState<string[]>([]);
+    const [link, setLink] = useState<string>('default');
+
+    const handleClick = () => {
+        if (link === 'default') {
+            window.alert('Invalid Link');
+            return;
+        }
+        fetch(`${HOST}/get-notes?=${link}`).then(
+            res => res.json()
+        ).then(
+            data => setNotes(data)
+        )
+    }
     return (
         <div className={`${containerSettings}`}>
-            <div className={``}>SubmitLink</div>
-            <button className={`${btnSettings}`}> GIVE ME MY NOTES!!! </button>
+            <input type='text' className={`${fieldSettings}`} onChange={e => setLink(e.target.value)} placeholder='Paste Link to Video here!'/>
+            <button className={`${btnSettings}`} onClick={() => handleClick()}> GIVE ME MY NOTES!!! </button>
         </div>
-    )
+    );
 }
 
 export default SubmitLink
